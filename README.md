bb364f8738edde27a61ca5b8d8427556763ea66d
André-Abush Clause <dev@andreabc.net>
2019-08-04 13:26:40 +0200

    Added HTML to Markdown conversion using html2markdown module + Added the possibility to copy the HTML source in clipboard + Python 2 only: restored — copy the formatted HTML — feature

--- README.md
+++ README.md
@@ -1,30 +1,24 @@
 # nvda-markdownForever
 A small NVDA add-on that provides preview of MarkDown or HTML contents in 
-any textarea
+any textarea. Also, you can select text to convert.
 
 * Authors: André-Abush Clause and contributors
 * License: GNU General Public License, version 2
 * Download [stable version][1]
 
-## Usage
-Gestures (in any textarea):
+## Commands
+- *NVDA+ALT+b*: Markdown to HTML conversion. The result is displayed in your default browser.
+- *NVDA+ALT+n*: Markdown to HTML conversion. The result is displayed in a virtual buffer of NVDA.
+- *NVDA+ALT+k*: HTML to markdown conversion
+- *NVDA+ALT+l*: Show the HTML source from Markdown.
+- *NVDA+SHIFT+h*: Copy the result to the clipboard from Markdown. One press: copy the HTML source. Two quick presses: copy the formatted HTML.
 
-- *NVDA+ALT+k*: preview of MarkDown or HTML through _NVDA_.
-- *NVDA+ALT+l*: preview of MarkDown or HTML through _your default browser_.
-- *NVDA+ALT+j*: view *HTML source* from *Markdown* through _NVDA_.
-
-Also, you can select text to convert.
-
-*Notes*:
-
-- you can remap these gestures in the input gestures configuration,
-  under _MarkDown Forever_ section.
-- the *preview in browser* is better than the *preview through NVDA* in
-  some cases. Indeed, the preview through NVDA uses *an old version of
-  Internet Explorer*. For this reason, some tags such as `<details>`
-  are not supported.
+*Note*: you can remap these gestures in the input gestures configuration,
+        under _MarkDown Forever_ section.
 
 ## Included dependencies
 - *markdown2*: <https://github.com/trentm/python-markdown2>
+* *html2markdown*: <https://pypi.org/project/html2markdown/>
+- *HtmlClipboard*: <http://code.activestate.com/recipes/474121-getting-html-from-the-windows-clipboard/>
 
 [1]: https://andreabc.net/projects/NVDA_addons/MarkdownForever/latest
--- addon/globalPlugins/markdownForever/__init__.py
+++ addon/globalPlugins/markdownForever/__init__.py
@@ -12,6 +12,9 @@ from __future__ import unicode_literals
 import os
 import sys
 isPy3 = True if sys.version_info >= (3, 0) else False
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib/common"))
+if isPy3: sys.path.append(os.path.join(os.path.dirname(__file__), "lib/py3"))
+else: sys.path.append(os.path.join(os.path.dirname(__file__), "lib/py2"))
 import re
 import time
 
@@ -19,23 +22,25 @@ import addonHandler
 addonHandler.initTranslation()
 import api
 import globalPluginHandler
+import scriptHandler
 import textInfos
 import treeInterceptorHandler
 import ui
 from logHandler import log
 
-from . import markdown2
-
+import markdown2
+import html2markdown
+if not isPy3: import htmlClipboard
 template_HTML = """
 <!DOCTYPE HTML>
 <html>
-    <head>
-        <meta charset="UTF-8" />
-        <title>{title}</title>
-    </head>
-    <body>
-        {body}
-    </body>
+	<head>
+		<meta charset="UTF-8" />
+		<title>{title}</title>
+	</head>
+	<body>
+		{body}
+	</body>
 </html>
 """
 
@@ -90,28 +95,66 @@ def convert(text, save=False, html=True):
 			html
 		)
 
+def copyToClipAsHTML():
+	text = getText()
+	if text:
+		body, toc = md2HTML(text)
+		htmlClipboard.PutHtml(body)
+		if htmlClipboard.GetHtml() == body: ui.message(_("Result copied in clipboard"))
+		else: ui.message(_("An error occurred"))
+	else: ui.message(_("No text"))
+
+
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	scriptCategory = _("Markdown Forever")
 
-	def script_showSourceViaNVDA(self, gesture):
+	def script_md2htmlSrcInNVDA(self, gesture):
 		text = getText()
-		convert(text, False, False)
-	script_showSourceViaNVDA.__doc__ = _("Show HTML source from Markdown through NVDA")
+		if text: convert(text, False, False)
+		else: ui.message(_("No text"))
+	script_md2htmlSrcInNVDA.__doc__ = _("Show the HTML source from Markdown")
 
-	def script_showViaNVDA(self, gesture):
+	def script_html2md(self, gesture):
 		text = getText()
-		convert(text)
-	script_showViaNVDA.__doc__ = _("Preview of MarkDown or HTML through NVDA")
+		if text:
+			res = html2markdown.convert(text)
+			ui.browseableMessage(res, _("HTML to Markdown conversion"), False)
+		else: ui.message(_("No text"))
+	script_html2md.__doc__ = _("HTML to markdown conversion")
 
-	def script_showInBrowser(self, gesture):
+	def script_md2htmlInNVDA(self, gesture):
 		text = getText()
-		convert(text, True)
-	script_showInBrowser.__doc__ = _("Preview of MarkDown or HTML through your default browser")
+		if text: convert(text)
+		else: ui.message(_("No text"))
+	script_md2htmlInNVDA.__doc__ = _("Markdown to HTML conversion. The result is displayed in a virtual buffer of NVDA")
+
+	def script_md2htmlInBrowser(self, gesture):
+		text = getText()
+		if text: convert(text, True)
+		else: ui.message(_("No text"))
+	script_md2htmlInBrowser.__doc__ = _("Markdown to HTML conversion. The result is displayed in your default browser")
+
+	def script_copyToClip(self, gesture):
+		if scriptHandler.getLastScriptRepeatCount() == 0: 
+			text = getText()
+			if text:
+				res = md2HTML(text)[0]
+				if not isPy3: res = res.decode("UTF-8")
+				api.copyToClip(res)
+				ui.message("HTML source copied to clipboard")
+			else: ui.message(_("No text"))
+		else:
+			if isPy3: ui.message(_("The copy as formatted HTML is currently unavailable under Python 3"))
+			else:
+				copyToClipAsHTML()
+				ui.message(_("Formatted HTML copied to clipboard"))
+	script_copyToClip.__doc__ = _("Copy the result to the clipboard from Markdown. One press: copy the HTML source. Two quick presses: copy the formatted HTML")
 
 	__gestures = {
-		"kb:nvda+alt+j": "showSourceViaNVDA",
-		"kb:nvda+alt+k": "showViaNVDA",
-		"kb:nvda+alt+l": "showInBrowser"
+		"kb:nvda+alt+b": "md2htmlInBrowser",
+		"kb:nvda+alt+n": "md2htmlInNVDA",
+		"kb:nvda+alt+k": "md2htmlSrcInNVDA",
+		"kb:nvda+alt+l": "html2md",
+		"kb:nvda+shift+h": "copyToClip",
 	}
-	

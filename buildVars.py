ce23eaed780df891c89ff900d537fac92e909a04
André-Abush CLAUSE <dev@andreabc.net>
2024-01-03 06:43:01 +0100

    Update Add-on Template

--- buildVars.py
+++ buildVars.py
@@ -1,18 +1,33 @@
+# -*- coding: UTF-8 -*-
 import subprocess
 import time
-
-hashCommit = "unknown"
-out = subprocess.check_output(["git", "status", "--porcelain"]).strip().decode()
-if not out.strip():
-	label = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).strip().decode()
-	if len(hashCommit) == 7: hashCommit = label
-
 
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
-# Full getext (please don't change)
-_ = lambda x : x
+
+# Since some strings in `addon_info` are translatable,
+# we need to include them in the .po files.
+# Gettext recognizes only strings given as parameters to the `_` function.
+# To avoid initializing translations in this module we simply roll our own "fake" `_` function
+# which returns whatever is given to it as an argument.
+def _(arg):
+	return arg
+
+
+updateChannel = None
+hashCommit = None
+outBranchName = subprocess.check_output(
+	["git", "branch", "--show-current"]).strip().decode()
+out = subprocess.check_output(
+	["git", "status", "--porcelain"]).strip().decode()
+if not out.strip():
+	label = subprocess.check_output(
+		["git", "rev-parse", "--short", "HEAD"]).strip().decode()
+	if label and len(label) == 7:
+		hashCommit = label
+if outBranchName.strip():
+	updateChannel = "stable" if outBranchName in ["stable", "master"] else "dev"
 
 # Add-on information variables
 addon_info = {
@@ -27,28 +42,40 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("Full-featured Markdown and HTML converter for NVDA"),
 	# version
-	"addon_version" : time.strftime('%y.%m.%d:') + hashCommit,
+	"addon_version": time.strftime("%y.%m.%d"),
 	# Author(s)
 	"addon_author" : "André <dev@andreabc.net>, Sof <hellosof@gmail.com> and other contributors",
 	# URL for the add-on documentation support
-	"addon_url" : "https://andreabc.net/projects/NVDA_addons/MarkdownForever/",
+	"addon_url": "https://andreabc.net/projects/NVDA_addons/MarkdownForever",
+	# URL for the add-on repository where the source code can be found
+	"addon_sourceURL": "https://github.com/aaclause/nvda-markdownForever",
 	# Documentation file name
-	"addon_docFileName" : None,
-	# Minimum NVDA version supported (e.g. "2018.3")
-	"addon_minimumNVDAVersion" : "2019.3.0",
-	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2023.1",
-	# Add-on update channel (default is stable or None)
-	"addon_updateChannel" : None,
+	"addon_docFileName": None,
+	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
+	"addon_minimumNVDAVersion": "2023.1",
+	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
+	"addon_lastTestedNVDAVersion": "2024.1",
+	# Add-on update channel (default is None, denoting stable releases,
+	# and for development releases, use "dev".)
+	# Do not change unless you know what you are doing!
+	"addon_updateChannel": updateChannel,
+	# Add-on license such as GPL 2
+	"addon_license": "GPL v2",
+	# URL for the license document the ad-on is licensed under
+	"addon_licenseURL": "https://www.gnu.org/licenses/gpl-2.0.html",
 }
-
-
-import os.path
+if hashCommit:
+	addon_info["addon_version"] += '-' + hashCommit
 
 # Define the python files that are the sources of your add-on.
-# You can use glob expressions here, they will be expanded.
-pythonSources = [os.path.join("addon", "*.py"),
-os.path.join("addon", "globalPlugins", "markdownForever", "*.py")]
+# You can either list every file (using ""/") as a path separator,
+# or use glob expressions.
+# For example to include all files with a ".py" extension from the "globalPlugins" dir of your add-on
+# the list can be written as follows:
+# pythonSources = ["addon/globalPlugins/*.py"]
+# For more information on SCons Glob expressions please take a look at:
+# https://scons.org/doc/production/HTML/scons-user/apd.html
+pythonSources = ["addon/globalPlugins/markdownForever/*.py"]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
@@ -56,3 +83,15 @@ i18nSources = pythonSources + ["buildVars.py"]
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
 excludedFiles = []
+
+# Base language for the NVDA add-on
+# If your add-on is written in a language other than english, modify this variable.
+# For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
+baseLanguage = "en"
+
+# Markdown extensions for add-on documentation
+# Most add-ons do not require additional Markdown extensions.
+# If you need to add support for markup such as tables, fill out the below list.
+# Extensions string must be of the form "markdown.extensions.extensionName"
+# e.g. "markdown.extensions.tables" to add tables.
+markdownExtensions = []
